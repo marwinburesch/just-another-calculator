59b726d9c61927c40cd25b069e6d74497f61d475
marwinburesch <marwin@posteo.de>
2019-11-28 16:01:26 +0100

    Add @emotion/core/styled
    add basic frame for calculator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import React from "react";
+import styled from "@emotion/styled";
+import GlobalStyles from "./GlobalStyles";
+
+const colors = {
+  base: "#00232E",
+  lining: "#46595A"
+};
+
+const ContentWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  background: ${colors.base};
+`;
+
+const Calculator = styled.div`
+  width: 300px;
+  height: 500px;
+  border: solid ${colors.lining};
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <ContentWrapper>
+        <Calculator />
+      </ContentWrapper>
+    </>
+  );
 }
 
 export default App;
